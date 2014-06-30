2eea3d3c5915e17816c9b0060373c95c85b34254
shihang <study1228.sh@gmail.com>
2014-06-30 20:48:53 +0800

    故意给FizzBuzz引入一个Bug，当调用2以后会导致测试3失败

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -2,10 +2,19 @@ package com.baldurtech;
 
 public class FizzBuzz
 {
+	boolean thisIsABug = false;
 	public String say(Integer num)
 	{
+		if(num == 2)
+		{
+			thisIsABug = true;
+		}
 		if(num % 3 == 0)
 		{
+			if(thisIsABug)
+			{
+				return "42";
+			}
 			return "Fizz";
 		}
 		return String.valueOf(num);
