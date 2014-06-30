cfccdd28bb24398e71c6acc18b5683fac213872b
shihang <study1228.sh@gmail.com>
2014-06-30 13:20:14 +0800

    将测试的程序从FizzBuzz类中分离出来

---
+++ src/main/java/com/baldurtech/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package com.baldurtech;
+
+public class FizzBuzzTest
+{
+	public static void main(String[] args)
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		System.out.println("1".equals(fizzBuzz.say(1)));
+	}
+}
