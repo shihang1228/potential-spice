2ce0add5ff47629fb39af9fe6be65c05c68c3aeb
shihang <study1228.sh@gmail.com>
2014-06-30 20:40:25 +0800

    将FizzBuzzTest编译后的文件放到test_classes文件夹中

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p target/classes
+mkdir -p target/test_classes
 javac -d target/classes src/main/java/com/baldurtech/*.java
-javac -d target/classes -classpath target/classes src/test/java/com/baldurtech/*.java
+javac -d target/test_classes -classpath target/classes src/test/java/com/baldurtech/*.java
