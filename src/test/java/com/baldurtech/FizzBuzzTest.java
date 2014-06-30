5331e4618286b38bdb292f81d6dd08b522c55a0b
shihang <study1228.sh@gmail.com>
2014-06-30 22:33:11 +0800

    把查找以test开头的方法和执行test的代码分成两个循环

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -1,21 +1,28 @@
 package com.baldurtech;
 
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ArrayList;
 
 public class FizzBuzzTest
 {
 	static boolean testResult = true;
 	public static void main(String[] args)throws Exception
-	{			
+	{
+		List<Method> testMethods = new ArrayList<Method>();
 		Method[] methods = FizzBuzzTest.class.getDeclaredMethods();
 		for(Method method: methods)
 		{
 			if(method.getName().startsWith("test"))
 			{
-				System.out.println("testing:" + method.getName());
-				Object obj = FizzBuzzTest.class.newInstance();
-				method.invoke(obj,new Object[]{});
-			}
+				testMethods.add(method);
+			} 
+		}
+		for(Method method: testMethods)
+		{
+			System.out.println("testing:" + method.getName());
+			Object obj = FizzBuzzTest.class.newInstance();
+			method.invoke(obj,new Object[]{});
 		}
 		outputTestReport();
 	}
